1dd60187eace5615616da36af9f07b1c959a24be
Moreaux19 <basargin19@gmail.com>
2025-02-14 19:02:05 +0300

    Three.js install path changed

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as THREE from '../node_modules/three/build/three.module.js';
+import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
 import { toggleDropdown } from './dropdown.js';
 
 // Переменные с HTML элементами
